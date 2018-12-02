663cb111fba1081ec1e9aaff42360269735eb6d6
Hryhorii <batulin.gr@gmail.com>
2018-12-02 21:11:08 +0200

    Update Eulerian path.py

--- Eulerian path.py
+++ Eulerian path.py
@@ -8,4 +8,24 @@ def Nzero(N):
         return False
     else:
         return True
-
+<<<<<<< HEAD
+def eiler(graph,rez[],x=0,y=0):
+    if Nzero(graph)==0:
+        return rez
+    while Nzero(graph):
+        while x<3:
+            while y<3:
+                if N[x][y]==1:
+                    rez.append([x,y])
+                    N[x][y]=0
+                    N[y][x]=0
+                    x=y
+                    break
+                y+=1
+            
+=======
+Part2
+code blah-blah-blah
+Part3
+code Ya-ya-ya-ya
+>>>>>>> parent of 0916ef0... Update Eulerian path.py
